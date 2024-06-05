69de84f7ddf2e5169292fa20808e0f4178a881a3
cnxqareviewer <cnxqareviewer@gmail.com>
2024-06-05 17:29:45 +0530

    adding new tc in branchA

--- TestProject/src/test/java/testcases/Test2.java
+++ TestProject/src/test/java/testcases/Test2.java
@@ -5,19 +5,18 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-    int strLength = str.length();
+		String str = "Radar", reverseStr = "";
+		int strLength = str.length();
 
-    for (int i = (strLength - 1); i >=0; --i) {
-      reverseStr = reverseStr + str.charAt(i);
-    }
+		for (int i = (strLength - 1); i >= 0; --i) {
+			reverseStr = reverseStr + str.charAt(i);
+		}
 
-    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
-      System.out.println(str + " is a Palindrome String.");
-    }
-    else {
-      System.out.println(str + " is not a Palindrome String.");
-    }
+		if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+			System.out.println(str + " is a Palindrome String.");
+		} else {
+			System.out.println(str + " is not a Palindrome String.");
+		}
 
 	}
 
